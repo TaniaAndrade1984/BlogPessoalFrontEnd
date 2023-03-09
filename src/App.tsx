acd76f0d1102324c86b979beba24b0d7fa187606
Tânia Andrade <120664909+TaniaAndrade1984@users.noreply.github.com>
2023-03-08 23:34:02 -0300

    React 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import Footer from './components/estaticos/footer/Footer';
+import Navbar from './components/estaticos/navbar/Navbar';
 import Login from './paginas/login/Login';
-
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
     <Router>
       <Navbar />
+        <div style={{ minHeight: "100vh" }}>
       <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/home" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
       </Routes>
+        </div>
       <Footer />
     </Router>
-
   );
 }
 
-export default App;
+export default App;
---
+++ src/services/Service.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://blogdothiagofaccipieri.onrender.com/'
+})
+
+export const login = async(url:any, dados:any, setDado:any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data.token)
+}
+export const cadastro = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
+    setDado(resposta.data.token)
+    }
