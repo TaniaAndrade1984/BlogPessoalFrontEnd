17e147bd26e2929b24bee31c80935d5e2479e6ff
Tânia Andrade <120664909+TaniaAndrade1984@users.noreply.github.com>
2023-03-11 11:49:11 -0300

    React 7

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import {Box} from '@mui/material';
 import Tema from '../../../models/Tema';
 import './ListaTema.css';
@@ -8,72 +8,71 @@ import useLocalStorage from 'react-use-localstorage';
 import {useNavigate} from 'react-router-dom';
 import { busca } from '../../../services/Service';
 
-
 function ListaTema() {
-    const [temas, setTemas] = useState<Tema[]>([])
-    const [token, setToken] = useLocalStorage('token');
-    let navigate = useNavigate();
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let navigate = useNavigate();
 
-    useEffect(()=>{
+  useEffect(()=>{
     if(token == ''){
-    alert("Você precisa estar logado")
-    navigate("/login")
+      alert("Você precisa estar logado")
+      navigate("/login")
     }
-    }, [token])
+  }, [token])
 
 
-    async function getTema(){
+  async function getTema(){
     await busca("/tema", setTemas, {
-    headers: {
+      headers: {
         'Authorization': token
-    }
+      }
     })
-    }
+  }
 
 
-    useEffect(()=>{
+  useEffect(()=>{
     getTema()
-    }, [temas.length])
+  }, [temas.length])
 
-    return (
+  return (
     <>
     {
-    temas.map(tema =>(
-    <Box m={2} >
+      temas.map(tema =>(
+      <Box m={2} >
         <Card variant="outlined">
-        <CardContent>
+          <CardContent>
             <Typography color="textSecondary" gutterBottom>
-            Tema
+              Tema
             </Typography>
             <Typography variant="h5" component="h2">
-            {tema.descricao}
+             {tema.descricao}
             </Typography>
-            </CardContent>
-            <CardActions>
+          </CardContent>
+          <CardActions>
             <Box display="flex" justifyContent="center" mb={1.5} >
 
-            <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
-                <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
-                </Button>
+                  </Button>
                 </Box>
-                </Link>
-                <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+              </Link>
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
-                <Button variant="contained" size='small' color="secondary">
+                  <Button variant="contained" size='small' color="secondary">
                     deletar
-                </Button>
+                  </Button>
                 </Box>
-                </Link>
+              </Link>
             </Box>
-            </CardActions>
+          </CardActions>
         </Card>
-        </Box>
-        ))
-        }
+      </Box>
+      ))
+      }
     </>
-    );
+  );
 }
 
 
